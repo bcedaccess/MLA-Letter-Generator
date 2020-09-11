a8e57f08787b6c3a8e3bc954f181cb0bc0b0f434
e-newton <ericnewton2251@gmail.com>
2020-09-11 12:43:03 -0700

    Created blank app html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('MLA-Letter-Generator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MLA-Letter-Generator app is running!');
-  });
+
 });
