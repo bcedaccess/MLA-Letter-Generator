ca57e8d84f321c85fdc0badb7b8175b4343bcacc
e-newton <ericnewton2251@gmail.com>
2020-09-12 12:02:38 -0700

    Added input container reference to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {InputcontainerComponent} from './components/inputcontainer/inputcontainer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'MLA-Letter-Generator';
+
+
+  @ViewChild(InputcontainerComponent, {static: false}) inputs: InputcontainerComponent;
+  ngAfterViewInit(): void {
+    console.log('Hello ', this.inputs.email);
+  }
 }
