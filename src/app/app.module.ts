9a2ae66dc1cd1198f4971cfdfc7bbd4f2cb9fd7d
e-newton <ericnewton2251@gmail.com>
2020-09-12 13:46:37 -0700

    Started API call to get reps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 import { SubmitbuttonComponent } from './components/submitbutton/submitbutton.component';
 import {MatButtonModule} from '@angular/material/button';
 import { LetterareaComponent } from './components/letterarea/letterarea.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { LetterareaComponent } from './components/letterarea/letterarea.componen
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
