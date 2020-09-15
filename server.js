2df3ab4f0d5b14566d88d2287f6266e36e339f25
e-newton <ericnewton2251@gmail.com>
2020-09-15 14:57:26 -0700

    Changes to server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mla-letter-generator'));
+app.use(express.static(__dirname + '/dist/MLA-Letter-Generatorr'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/mla-letter-generator/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/MLA-Letter-Generator/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
