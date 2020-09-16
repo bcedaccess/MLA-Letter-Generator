4b19c5db3d567ffe52db156a4f79ccb7c4a314b0
e-newton <ericnewton2251@gmail.com>
2020-09-15 19:26:16 -0700

    Code Cleaning

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 
 const app = express();
@@ -23,7 +23,7 @@ if(!process.env.PRODUCTION){
 }
 app.use(express.json());
 
-var transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   auth: {
     user: process.env.EMAIL_USER,
